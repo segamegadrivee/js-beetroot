fc705e1880b321885ed9b9769159500d1eba8ea6
sega <sergohack2015@gmail.com>
2023-01-31 18:10:36 +0200

    generator

--- randomUser/main.js
+++ randomUser/main.js
@@ -4,6 +4,7 @@ let headerButton = document.querySelector(".header__button");
 let out = document.querySelector('.main');
 
 
+
 slider.oninput = function () {
     count.innerHTML = this.value;
 }
@@ -11,14 +12,29 @@ slider.oninput = function () {
 
 
 function createCard(quantity) {
-
     for (let i = 0; i < quantity; i++) {
-        out.innerHTML +=
-            `  <div class="main__usercard">
+        fetch('https://randomuser.me/api/?inc=name,location,picture,nat')
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
 
-              </div>
-        `
+                console.log(data);
+                out.innerHTML += `<div class="main__usercard">
+                        <img class="usercard__avatar" src="${data.results[0].picture.large}"
+                            alt="" height="100" width="100">
+
+                        <span class="usercard__fullname">${data.results[0].name.first} ${data.results[0].name.last}</span>
+
+                        <div class="usercard__region">
+                            <span class="usercard__country">${data.results[0].location.country} , ${data.results[0].location.city}</span>
+                            <img class="usercard__flag" src="https://flagsapi.com/${data.results[0].nat}/flat/64.png"" alt="" height="30">
+                        </div>
+                    </div>`;
+
+            });
     }
+
 }
 
 
@@ -36,3 +52,17 @@ headerButton.onclick = () => {
 
 
 
+// for (let i = 0; i < quantity; i++) {
+//     out.innerHTML +=
+//         `  <div class="main__usercard">
+//                 <img class="usercard__avatar" src="https://e7.pngegg.com/pngimages/273/154/png-clipart-cats-cats.png"
+//                 alt="" height="100" width="100">
+
+//             <span class="usercard__fullname">Stepan Cat</span>
+
+//             <div class="usercard__region">
+//                 <span class="usercard__country">Ukraine , Kyiv</span>
+//                 <img class="usercard__flag" src="https://flagsapi.com/UA/flat/64.png" alt="" height="30">
+//           </div>
+//     `
+// }
