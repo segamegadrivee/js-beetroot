122fccf774d5511b1c56474ba0da495172270664
sega <sergohack2015@gmail.com>
2023-02-07 23:18:59 +0200

    gfdg

--- randomUser/main.js
+++ randomUser/main.js
@@ -1,9 +1,9 @@
 let slider = document.querySelector(".header__range");
 let count = document.querySelector(".header__counter");
+let headerCountry = document.querySelector('.header__country');
 let headerButton = document.querySelector(".header__button");
 let out = document.querySelector('.main');
 let users = [];
-
 
 
 
@@ -11,50 +11,49 @@ slider.oninput = function () {
     count.innerHTML = this.value;
 }
 
+headerButton.onclick = () => {
+    createCards(slider.value)
+
+}
+
+
+
 
 function removeDuplicates(arr) {
     let uniquearray = Array.from(new Set(arr))
     return uniquearray
 }
 
-function createCard(quantity) {
-    for (let i = 0; i < quantity; i++) {
-        fetch('https://randomuser.me/api/?inc=name,location,picture,nat')
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-
-                console.log(data);
+function createCards(quantity) {
+    fetch(`https://randomuser.me/api/?results=${quantity}&inc=name,location,picture,nat&noinfo`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            for (let i = 0; i < quantity; i++) {
                 out.innerHTML += `<div class="main__usercard">
-                        <img class="usercard__avatar" src="${data.results[0].picture.large}"
+                        <img class="usercard__avatar" src="${data.results[i].picture.large}"
                             alt="" height="100" width="100">
 
-                        <span class="usercard__fullname">${data.results[0].name.first} ${data.results[0].name.last}</span>
+                        <span class="usercard__fullname">${data.results[i].name.first} ${data.results[i].name.last}</span>
 
                         <div class="usercard__region">
-                            <span class="usercard__country">${data.results[0].location.country} , ${data.results[0].location.city}</span>
-                            <img class="usercard__flag" src="https://flagsapi.com/${data.results[0].nat}/flat/64.png"" alt="" height="30">
+                            <span class="usercard__country">${data.results[i].location.country} , ${data.results[i].location.city}</span>
+                            <img class="usercard__flag" src="https://flagsapi.com/${data.results[i].nat}/flat/64.png"" alt="" height="30">
                         </div>
                     </div>`;
 
-                users.push(data.results[0].location.country);
 
-
-
-            });
-    }
-
-}
-
-
-
-
-headerButton.onclick = () => {
-    createCard(slider.value)
-
+                users.push(data.results[i].location.country);;
+            }
+            console.log(removeDuplicates(users));
+        });
 
 }
+
+
+
 
 
 
