43f7408ee616dcf9f321eca6315e24b3d9abf68d
sega <sergohack2015@gmail.com>
2023-02-09 02:43:49 +0200

    new

--- randomUser/main.js
+++ randomUser/main.js
@@ -3,8 +3,7 @@ let count = document.querySelector(".header__counter");
 let headerCountry = document.querySelector('.header__country');
 let headerButton = document.querySelector(".header__button");
 let out = document.querySelector('.main');
-let users = [];
-
+let usersCountry = [];
 
 
 slider.oninput = function () {
@@ -12,17 +11,16 @@ slider.oninput = function () {
 }
 
 headerButton.onclick = () => {
+    console.log('click');
     createCards(slider.value)
-
 }
-
-
 
 
 function removeDuplicates(arr) {
     let uniquearray = Array.from(new Set(arr))
     return uniquearray
 }
+
 
 function createCards(quantity) {
     fetch(`https://randomuser.me/api/?results=${quantity}&inc=name,location,picture,nat&noinfo`)
@@ -31,31 +29,136 @@ function createCards(quantity) {
         })
         .then((data) => {
             console.log(data);
-            for (let i = 0; i < quantity; i++) {
-                out.innerHTML += `<div class="main__usercard">
-                        <img class="usercard__avatar" src="${data.results[i].picture.large}"
-                            alt="" height="100" width="100">
 
-                        <span class="usercard__fullname">${data.results[i].name.first} ${data.results[i].name.last}</span>
 
-                        <div class="usercard__region">
-                            <span class="usercard__country">${data.results[i].location.country} , ${data.results[i].location.city}</span>
-                            <img class="usercard__flag" src="https://flagsapi.com/${data.results[i].nat}/flat/64.png"" alt="" height="30">
-                        </div>
+            function renderUser(i) {
+                    out.innerHTML += `<div class="main__usercard">
+                    <img class="usercard__avatar" src="${data.results[i].picture.large}"
+                        alt="" height="100" width="100">
+                
+                    <span class="usercard__fullname">${data.results[i].name.first} ${data.results[i].name.last}</span>
+                
+                    <div class="usercard__region">
+                        <span class="usercard__country">${data.results[i].location.country} , ${data.results[i].location.city}</span>
+                        <img class="usercard__flag" src="https://flagsapi.com/${data.results[i].nat}/flat/64.png"" alt="" height="30">
+                    </div>
                     </div>`;
-
-
-                users.push(data.results[i].location.country);;
             }
-            console.log(removeDuplicates(users));
-        });
 
+ 
+           for (let i = 0; i < quantity; i++) {
+                renderUser(i)
+                usersCountry.push(data.results[i].location.country);
+            
+           }
+           
+
+            removeDuplicates(usersCountry).forEach(element => 
+            headerCountry.innerHTML += `<option>${element}</option>` )
+
+            console.log(removeDuplicates(usersCountry));
+
+            
+            headerCountry.oninput = () => {
+                out.innerHTML = '';
+                for (let i = 0; i < quantity; i++) {
+                    if (headerCountry.value == data.results[i].location.country) {
+                        renderUser(i)
+                    } 
+                                     
+                    if (headerCountry.value == 'All') {
+                        renderUser(i)
+                        console.log("ALL");
+                    }  
+                }
+            }
+            
+            
+
+        });
+    return
 }
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// createCards(slider.value)
+// function removeDuplicates(arr) {
+//     let uniquearray = Array.from(new Set(arr))
+//     return uniquearray
+// }
+
+// function createCards(quantity) {
+//     fetch(`https://randomuser.me/api/?results=${quantity}&inc=name,location,picture,nat&noinfo`)
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .then((data) => {
+//             console.log(data);
+//             for (let i = 0; i < quantity; i++) {
+
+//                 function renderCard(i) {
+                    // out.innerHTML += `<div class="main__usercard">
+                    // <img class="usercard__avatar" src="${data.results[i].picture.large}"
+                    //     alt="" height="100" width="100">
+                
+                    // <span class="usercard__fullname">${data.results[i].name.first} ${data.results[i].name.last}</span>
+                
+                    // <div class="usercard__region">
+                    //     <span class="usercard__country">${data.results[i].location.country} , ${data.results[i].location.city}</span>
+                    //     <img class="usercard__flag" src="https://flagsapi.com/${data.results[i].nat}/flat/64.png"" alt="" height="30">
+                    // </div>
+                    // </div>`;
+//                 }
+
+
+//                 renderCard(i)
+//                 usersCountry.push(data.results[i].location.country);;
+
+
+
+//             }
+//             usersCountry = removeDuplicates(usersCountry);
+//             console.log(usersCountry);
+//         });
+//     return
+// }
+
+
+
+
+// function sortCountry() {
+
+// }
 
 
 
